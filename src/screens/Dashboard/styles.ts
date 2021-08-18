1dc703ec95eb534bd2401c93c9150b3f76757bb9
Rodrigo Araujo <rodrigoaraujo990@gmail.com>
2021-08-17 23:44:25 -0300

    add provider theme

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,15 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #999;
-`;
 
+  background-color: ${({ theme }) => theme.colors.primary};
+`; 
 
 export const Title = styled.Text`
-
+  font-size: 24px;
+  color: purple;
+  font-weight: bold;
 `;
