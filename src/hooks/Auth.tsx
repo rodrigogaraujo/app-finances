9f26f12c6156ea0ce86c00969746c30735a8be9a
Rodrigo Araujo <rodrigoaraujo990@gmail.com>
2021-08-25 00:29:38 -0300

    small fixsz

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -106,10 +106,12 @@ function AuthProvider({ children }: AuthProviderProps) {
         ],
       });
       if (credential) {
+        const name = credential.fullName!.givenName!;
+        const photo = `https://ui.avatars.com/api/?name=${name}&length=1`;
         const userLogged = {
           id: String(credential.user),
           email: credential.email!,
-          name: credential.fullName!.givenName!,
+          name,
           photo: undefined,
         };
         setUser(userLogged);
