9e4c7ca0405b4131df8ce1b619befe4d9bf161fc
Rodrigo Araujo <rodrigoaraujo990@gmail.com>
2021-08-24 21:53:36 -0300

    init sign and type svg

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import AppLoading from "expo-app-loading";
 import { NavigationContainer } from "@react-navigation/native";
 
 import theme from "./src/global/styles/theme";
-import { AppRoutes } from "./src/routes/app.routes";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +32,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,43 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Header = styled.View`
+  width: 100%;
+  height: 70%;
+  background-color: ${({theme}) => theme.colors.primary};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TitleWrapper = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.medium};
+  font-size: ${RFValue(30)}px;
+  color: ${({theme}) => theme.colors.shape};
+  text-align: center;
+  margin-top: ${RFValue(45)}px;
+`;
+
+export const SignInTitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+  color: ${({theme}) => theme.colors.shape};
+  text-align: center;
+  margin-top: ${RFValue(80)}px;
+  margin-bottom: ${RFValue(67)}px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  height: 30%;
+  background-color: ${({theme}) => theme.colors.secondary};
+  align-items: center;
+  justify-content: center;
+`;
